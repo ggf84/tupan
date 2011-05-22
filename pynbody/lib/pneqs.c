7d78ca56847b672be82c8ce5eab6df45320cadd2
ggf84 <gg.ferrari@gmail.com>
2011-05-22 04:26:39 -0300

    implemented interface for get_pnacc.

--- pynbody/lib/pneqs.c
+++ pynbody/lib/pneqs.c
@@ -1,10 +1,54 @@
 #include "Python.h"
+#include "numpy/arrayobject.h"
 #include<math.h>
 
 
-double get_pnacc(double a)
+typedef struct vector_struct {
+  double x;
+  double y;
+  double z;
+} VECT, *pVECT;
+
+
+typedef struct clight_struct {
+  double inv1;
+  double inv2;
+  double inv3;
+  double inv4;
+  double inv5;
+  double inv6;
+  double inv7;
+} CLIGHT, *pCLIGHT;
+
+
+
+
+VECT get_pnacc(unsigned long nj,
+               unsigned long *jindex_array,
+               double *jpos_array,
+               double *jvel_array,
+               double *jspin_array,
+               unsigned long *iindex,
+               double *ipos,
+               double *ivel,
+               double *ispin,
+               CLIGHT clight
+              )
 {
-    return sqrt(a);
+    VECT pnacc = {0.0, 0.0, 0.0};
+
+    pnacc.x = jpos_array[10];
+    pnacc.y = iindex[2];
+    pnacc.z = nj;
+
+    int j, k;
+    for (j = 0; j < nj; ++j) {
+        for (k = 0; k < 3; ++k) {
+            printf("%f\n", jpos_array[3*j+k]);
+        }
+    }
+
+    return pnacc;
 }
 
 
@@ -12,15 +56,48 @@ double get_pnacc(double a)
 static PyObject *pneqsError;
 
 
-static PyObject *_get_pnacc(PyObject *_self, PyObject *_a)
+#define cast_to_ulong(p) ((unsigned long *) (((PyArrayObject *)p)->data))
+#define cast_to_double(p) ((double *) (((PyArrayObject *)p)->data))
+
+static PyObject *_get_pnacc(PyObject *_self, PyObject *_args)
 {
-    double a = PyFloat_AsDouble(_a);
-    return Py_BuildValue("dd", a, get_pnacc(a));
+    CLIGHT clight;
+    unsigned long nj, *iindex, *jindex_array;
+    double *ipos, *ivel, *ispin;
+    double *jpos_array, *jvel_array, *jspin_array;
+    PyObject *_iindex, *_ipos, *_ivel, *_ispin;
+    PyObject *_jindex_array, *_jpos_array, *_jvel_array, *_jspin_array;
+
+    PyArg_ParseTuple(_args,"kOOOOOOOOddddddd", &nj,
+                            &_jindex_array, &_jpos_array,
+                            &_jvel_array, &_jspin_array,
+                            &_iindex, &_ipos, &_ivel, &_ispin,
+                            &clight.inv1, &clight.inv2, &clight.inv3,
+                            &clight.inv4, &clight.inv5, &clight.inv6,
+                            &clight.inv7);
+
+    iindex = cast_to_ulong(_iindex);
+    ipos = cast_to_double(_ipos);
+    ivel = cast_to_double(_ivel);
+    ispin = cast_to_double(_ispin);
+    jindex_array = cast_to_ulong(_jindex_array);
+    jpos_array = cast_to_double(_jpos_array);
+    jvel_array = cast_to_double(_jvel_array);
+    jspin_array = cast_to_double(_jspin_array);
+
+    VECT pnacc = get_pnacc(nj,
+                           jindex_array, jpos_array,
+                           jvel_array, jspin_array,
+                           iindex, ipos, ivel, ispin,
+                           clight
+                          );
+
+    return Py_BuildValue("ddd", pnacc.x, pnacc.y, pnacc.z);
 }
 
 
 static PyMethodDef pneqs_meths[] = {
-    {"get_pnacc", (PyCFunction)_get_pnacc, METH_O,
+    {"get_pnacc", (PyCFunction)_get_pnacc, METH_VARARGS,
                   "returns PN corrections of accel."},
     {NULL, NULL, 0, NULL},
 };
