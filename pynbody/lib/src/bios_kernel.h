0780bfa282b781c090e41248f154c56a54348475
ggf84 <gg.ferrari@gmail.com>
2012-06-26 18:35:55 +0200

    added a TTL core for BIOS integrator.

--- pynbody/lib/src/bios_kernel.h
+++ pynbody/lib/src/bios_kernel.h
@@ -1,6 +1,7 @@
-#ifndef BIOS_H
-#define BIOS_H
+#ifndef BIOS_KERNEL_H
+#define BIOS_KERNEL_H
 
+#include <stdlib.h>
 #include"common.h"
 #include"smoothing.h"
 
@@ -11,6 +12,19 @@
  */
 
 
+inline REAL
+get_phi(const REAL m,
+        const REAL x,
+        const REAL y,
+        const REAL z,
+        const REAL eps2)
+{
+    REAL r2 = x * x + y * y + z * z;                                 // 5 FLOPs
+    REAL inv_r = phi_smooth(r2, eps2);                               // 3 FLOPs
+    return m * inv_r;                                                // 1 FLOPs
+}
+
+
 inline REAL3
 get_acc(const REAL m,
         const REAL x,
@@ -19,8 +33,8 @@ get_acc(const REAL m,
         const REAL eps2)
 {
     REAL r2 = x * x + y * y + z * z;                                 // 5 FLOPs
-    REAL m_r3 = acc_smooth(r2, eps2);                                // 5 FLOPs
-    m_r3 *= m;                                                       // 1 FLOPs
+    REAL inv_r3 = acc_smooth(r2, eps2);                              // 4 FLOPs
+    REAL m_r3 = m * inv_r3;                                          // 1 FLOPs
     REAL3 a;
     a.x = -m_r3 * x;                                                 // 1 FLOPs
     a.y = -m_r3 * y;                                                 // 1 FLOPs
@@ -30,10 +44,13 @@ get_acc(const REAL m,
 
 
 inline REAL8
-twobody_solver(const REAL dt,
-               REAL4 r,
-               REAL4 v)
+leapfrog(const REAL dt,
+         const REAL4 r0,
+         const REAL4 v0)
 {
+    REAL4 r = r0;
+    REAL4 v = v0;
+
     r.x += v.x * dt/2;
     r.y += v.y * dt/2;
     r.z += v.z * dt/2;
@@ -48,7 +65,81 @@ twobody_solver(const REAL dt,
     r.y += v.y * dt/2;
     r.z += v.z * dt/2;
 
-    return (REAL8){r.x, r.y, r.z, 0, v.x, v.y, v.z, 0};
+    return (REAL8){r.x, r.y, r.z, r.w, v.x, v.y, v.z, v.w};
+}
+
+
+inline REAL8
+TTL_core(const REAL dt,
+         const REAL u0,
+         const REAL k0,
+         const REAL4 r0,
+         const REAL4 v0,
+         REAL *t)
+{
+    REAL4 r = r0;
+    REAL4 v = v0;
+
+    REAL w0 = 2 * u0;
+    REAL tau0 = dt / w0;
+    *t += tau0;
+    r.x += v.x * tau0;
+    r.y += v.y * tau0;
+    r.z += v.z * tau0;
+
+    REAL r2 = r.x * r.x + r.y * r.y + r.z * r.z;
+    REAL inv_r2 = 1 / (r2 + v.w);
+    inv_r2 = (r2 > 0) ? (inv_r2):(0);
+
+    v.x -= r.x * inv_r2 * dt;
+    v.y -= r.y * inv_r2 * dt;
+    v.z -= r.z * inv_r2 * dt;
+    REAL k1 = (v.x * v.x + v.y * v.y + v.z * v.z)/2;
+
+    REAL u1 = u0 + (k1 - k0);
+    REAL w1 = 2 * u1;
+    REAL tau1 = dt / w1;
+    *t += tau1;
+    r.x += v.x * tau1;
+    r.y += v.y * tau1;
+    r.z += v.z * tau1;
+    return (REAL8){r.x, r.y, r.z, r.w, v.x, v.y, v.z, v.w};
+}
+
+
+inline REAL8
+TTL(const REAL dt,
+    const REAL4 r0,
+    const REAL4 v0)
+{
+    REAL4 r = r0;
+    REAL4 v = v0;
+
+    REAL k0 = (v.x * v.x + v.y * v.y + v.z * v.z)/2;
+    REAL u0 = get_phi(r.w, r.x, r.y, r.z, v.w);
+
+    REAL tau = u0 * dt;
+    REAL t = 0;
+    REAL8 rv_new = TTL_core(tau, u0, k0, r, v, &t);
+
+    while (fabs(t-dt)/dt > 5.9604644775390625E-8) {
+        tau *= sqrt(dt/t);
+        t = 0;
+        rv_new = TTL_core(tau, u0, k0, r, v, &t);
+    }
+
+    return rv_new;
+}
+
+
+inline REAL8
+twobody_solver(const REAL dt,
+               const REAL4 r0,
+               const REAL4 v0)
+{
+
+//    return leapfrog(dt, r0, v0);
+    return TTL(dt, r0, v0);
 }
 
 
@@ -72,16 +163,19 @@ bios_kernel_core(REAL8 iposvel,
     v.z = vi.z - vj.z;                                               // 1 FLOPs
     v.w = vi.w + vj.w;                                               // 1 FLOPs
     REAL mu = (ri.w * rj.w) / r.w;                                   // 2 FLOPs
+    if (r.x == 0 && r.y == 0 && r.z == 0) {
+        return iposvel;
+    }
 
     REAL8 rv_new = twobody_solver(dt, r, v);                         // ? FLOPS
 
-    iposvel.s0 += mu * ((rv_new.s0 - r.x) - v.x * dt);                // 4 FLOPs
-    iposvel.s1 += mu * ((rv_new.s1 - r.y) - v.y * dt);                // 4 FLOPs
-    iposvel.s2 += mu * ((rv_new.s2 - r.z) - v.z * dt);                // 4 FLOPs
+    iposvel.s0 += mu * ((rv_new.s0 - r.x) - v.x * dt);               // 4 FLOPs
+    iposvel.s1 += mu * ((rv_new.s1 - r.y) - v.y * dt);               // 4 FLOPs
+    iposvel.s2 += mu * ((rv_new.s2 - r.z) - v.z * dt);               // 4 FLOPs
     iposvel.s3  = 0;
-    iposvel.s4 += mu * (rv_new.s4 - v.x);                             // 2 FLOPs
-    iposvel.s5 += mu * (rv_new.s5 - v.y);                             // 2 FLOPs
-    iposvel.s6 += mu * (rv_new.s6 - v.z);                             // 2 FLOPs
+    iposvel.s4 += mu * (rv_new.s4 - v.x);                            // 2 FLOPs
+    iposvel.s5 += mu * (rv_new.s5 - v.y);                            // 2 FLOPs
+    iposvel.s6 += mu * (rv_new.s6 - v.z);                            // 2 FLOPs
     iposvel.s7  = 0;
 
     return iposvel;
@@ -176,5 +270,5 @@ _bios_kernel(PyObject *_args)
 #endif  // __OPENCL_VERSION__
 
 
-#endif  // BIOS_H
+#endif  // BIOS_KERNEL_H
 
