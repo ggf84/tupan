7a9f9a31e4fd92016b51027ab16808de14af4574
ggf84 <gg.ferrari@gmail.com>
2012-07-01 17:28:37 +0200

    removes old stuff from integrator, which includes the leapfrog.py file.

--- pynbody/integrator/__init__.py
+++ pynbody/integrator/__init__.py
@@ -6,18 +6,17 @@
 """
 
 
-from . import hts
+from . import sia
 from . import bios
 from . import hermite
-from . import leapfrog
 
 
 class Integrator(object):
     """
 
     """
-    PROVIDED_METHODS = ['leapfrog', 'adaptlf', 'bios', 'hermite', 'adapthermite']
-    PROVIDED_METHODS.extend(hts.SIA.PROVIDED_METHODS)
+    PROVIDED_METHODS = ['bios', 'hermite', 'adapthermite']
+    PROVIDED_METHODS.extend(sia.SIA.PROVIDED_METHODS)
 
     def __init__(self, eta, time, particles, **kwargs):
         import logging
@@ -26,24 +25,18 @@ class Integrator(object):
         method = kwargs.pop("method", None)
 
         self.integrator = None
-        if method == "leapfrog":
-            logger.info("Using 'leapfrog' integrator.")
-            self.integrator = leapfrog.LeapFrog(eta, time, particles, **kwargs)
-        elif method == "adaptlf":
-            logger.info("Using 'adaptlf' integrator.")
-            self.integrator = leapfrog.AdaptLF(eta, time, particles, **kwargs)
-        elif method == "bios":
-            logger.info("Using 'bios' integrator.")
-            self.integrator = bios.BIOS(eta, time, particles, **kwargs)
-        elif method in hts.SIA.PROVIDED_METHODS:
+        if method in sia.SIA.PROVIDED_METHODS:
             logger.info("Using %s integrator.", method)
-            self.integrator = hts.SIA(eta, time, particles, method, **kwargs)
+            self.integrator = sia.SIA(eta, time, particles, method, **kwargs)
         elif method == "hermite":
             logger.info("Using 'hermite' integrator.")
             self.integrator = hermite.Hermite(eta, time, particles, **kwargs)
         elif method == "adapthermite":
             logger.info("Using 'adapthermite' integrator.")
             self.integrator = hermite.AdaptHermite(eta, time, particles, **kwargs)
+        elif method == "bios":
+            logger.info("Using 'bios' integrator.")
+            self.integrator = bios.BIOS(eta, time, particles, **kwargs)
         else:
             logger.critical("Unexpected integrator method: '%s'. Provided methods: %s",
                             method, str(self.PROVIDED_METHODS))
