9804ef0735e7a98308cecb08781a5ceee4950ca1
ggf84 <gg.ferrari@gmail.com>
2012-08-30 15:50:35 +0200

    now the IO class has a method to take time slices from a PBaSS stream.

--- pynbody/io/__init__.py
+++ pynbody/io/__init__.py
@@ -113,4 +113,83 @@ class IO(object):
         raise ValueError('This file is already in \'hdf5\' format!')
 
 
+
+
+
+    def take_time_slices(self, times):
+        import numpy as np
+        from scipy import interpolate
+        from collections import OrderedDict
+
+
+#        #######################################################################
+#        import matplotlib.pyplot as plt
+#        from pynbody.io import IO
+#        p0 = IO('snapshots0.hdf5').load()
+#        p = IO('snapshots.hdf5').load()
+#
+#        index = p[p.nstep == p.nstep.max()].id.item()
+#        a0 = p0[p0.id == index]
+#        a = p[p.id == index]
+#        x = np.linspace(0,100,10000)
+#        k = 0
+#        f1 = interpolate.UnivariateSpline(a.t_curr, a.pos[:,k], s=0, k=1)
+#        f2 = interpolate.UnivariateSpline(a.t_curr, a.pos[:,k], s=0, k=2)
+#        f3 = interpolate.UnivariateSpline(a.t_curr, a.pos[:,k], s=0, k=3)
+#        f4 = interpolate.UnivariateSpline(a.t_curr, a.pos[:,k], s=0, k=4)
+#        f5 = interpolate.UnivariateSpline(a.t_curr, a.pos[:,k], s=0, k=5)
+#        y1 = f1(x)
+#        y2 = f2(x)
+#        y3 = f3(x)
+#        y4 = f4(x)
+#        y5 = f5(x)
+#        plt.plot(a0.t_curr, a0.pos[:,k], label='data0')
+#        plt.plot(a.t_curr, a.pos[:,k], label='data')
+#        plt.plot(x, y1, label='f1')
+#        plt.plot(x, y2, label='f2')
+#        plt.plot(x, y3, label='f3')
+#        plt.plot(x, y4, label='f4')
+#        plt.plot(x, y5, label='f5')
+#        plt.legend(loc='upper right', shadow=True,
+#                   fancybox=True, borderaxespad=0.75)
+#        plt.show()
+#        #######################################################################
+
+
+        p = self.load()
+
+        snaps = OrderedDict()
+        for time in times:
+            snaps[time] = type(p)()
+
+        for key, obj in p.items:
+            if obj.n:
+                n = int(obj.id.max())+1
+
+                snap = type(obj)(n)
+                for time in times:
+                    snaps[time].append(snap)
+
+                for i in xrange(n):
+                    stream = obj[obj.id == i]
+                    t_curr = stream.t_curr
+                    for name in obj.names:
+                        attr = getattr(stream, name)
+                        if attr.ndim > 1:
+                            for k in xrange(attr.shape[1]):
+                                f = interpolate.UnivariateSpline(t_curr, attr[:,k], s=0, k=3)
+                                for time in times:
+                                    getattr(getattr(snaps[time], key), name)[i,k] = f(time)
+                        else:
+                            f = interpolate.UnivariateSpline(t_curr, attr[:], s=0, k=3)
+                            for time in times:
+                                getattr(getattr(snaps[time], key), name)[i] = f(time)
+
+        return snaps.values()
+
+
+
+
+
+
 ########## end of file ##########
