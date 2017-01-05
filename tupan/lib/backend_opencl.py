3dd10870d2fb4453b49e11f22355dd67b65f132b
ggf84 <gg.ferrari@gmail.com>
2017-01-05 02:58:19 -0200

    Revert previous commit. Instead, choose simd width based on fpwidth.

--- tupan/lib/backend_opencl.py
+++ tupan/lib/backend_opencl.py
@@ -167,14 +167,14 @@ class Program(object):
         simd = (self.cl_device.preferred_vector_width_float
                 if fpwidth == 'fp32'
                 else self.cl_device.preferred_vector_width_double)
-        lsize = 2 if fpwidth == 'fp32' else 1
+        lsize = 1
         wsize = 1
         if self.cl_device.type == cl.device_type.CPU:
-            lsize *= 32
+            lsize *= 64
             wsize *= self.cl_device.max_compute_units*2
         if self.cl_device.type == cl.device_type.GPU:
-            simd *= 2
-            lsize *= 64
+            simd *= 2 if fpwidth == 'fp32' else 1
+            lsize *= 128
             wsize *= self.cl_device.max_compute_units*32
         fast_local_mem = True
 
