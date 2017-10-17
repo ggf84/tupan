6e423cfea74644e7a1bea6a72569eaf4430ae3d3
ggf84 <gg.ferrari@gmail.com>
2017-10-17 05:40:40 -0200

    add maschberger2013 IMF.

--- tupan/ics/imf.py
+++ tupan/ics/imf.py
@@ -10,14 +10,37 @@ from scipy.integrate import quad
 from scipy.optimize import fminbound
 
 
+def plot_imf(m, ml, mu, name):
+    import matplotlib.pyplot as plt
+    bins = np.logspace(
+        np.log10(ml),
+        np.log10(mu),
+        int(np.sqrt(len(m))),
+    )
+    plt.hist(
+        m,
+        log=True,
+        label=name,
+        bins=bins,
+        histtype='step'
+    )
+    plt.xscale('log')
+    plt.legend(loc='best')
+    plt.xlabel(r'$m\,\left[M_{\odot}\right]$')
+    plt.ylabel(r'$m\,p(m) = \frac{d\,N(\log\,m)}{d\,\log\,m}$')
+    plt.tight_layout()
+    plt.show()
+
+
 class IMFSample(object):
     """
 
     """
-    def __init__(self, imf_func, min_mlow, max_mhigh, mlow, mhigh):
+    def __init__(self, imf_func, min_mlow, max_mhigh, mlow, mhigh, name=''):
         """
 
         """
+        self.__name__ = name
         if mlow < min_mlow or mhigh > max_mhigh:
             raise ValueError('Mass range outer of limits'
                              ' [{0}:{1}].'.format(min_mlow, max_mhigh))
@@ -45,9 +68,10 @@ class IMFSample(object):
         self._sample = None
         self._mtot = None
 
-    def sample(self, n):
+    def sample(self, n, seed=None):
         size = n
         ran_mass = []
+        np.random.seed(seed)
         while len(ran_mass) < n:
             ran_imf = np.random.uniform(0.0, self.peak, size=size)
             ran_mass = np.exp(np.random.uniform(np.log(self.mlow),
@@ -56,6 +80,9 @@ class IMFSample(object):
             size *= int(1+n/len(ran_mass))
         self._sample = ran_mass[:n]
         self._mtot = float(np.sum(self._sample))
+
+        plot_imf(self._sample, 0.01, 150.0, self.__name__)
+
         return self._sample
 
 
@@ -73,7 +100,9 @@ class IMF(object):
         def imf_func(m):
             return (1.0+m)-m
 
-        return IMFSample(imf_func, min_mlow, max_mhigh, mlow, mhigh)
+        return IMFSample(imf_func, min_mlow,
+                         max_mhigh, mlow, mhigh,
+                         'equalmass')
 
     @staticmethod
     def salpeter1955(mlow, mhigh):
@@ -83,7 +112,9 @@ class IMF(object):
         def imf_func(m):
             return m**(-1.35)
 
-        return IMFSample(imf_func, min_mlow, max_mhigh, mlow, mhigh)
+        return IMFSample(imf_func, min_mlow,
+                         max_mhigh, mlow, mhigh,
+                         'salpeter1955')
 
     @staticmethod
     def padoan2007(mlow, mhigh):
@@ -98,7 +129,9 @@ class IMF(object):
             return ((m**(-gamma)) * (1.0 + erf((
                 4.0 * np.log(m/m_ch) + sigma**2) / (np.sqrt(8.0) * sigma))))
 
-        return IMFSample(imf_func, min_mlow, max_mhigh, mlow, mhigh)
+        return IMFSample(imf_func, min_mlow,
+                         max_mhigh, mlow, mhigh,
+                         'padoan2007')
 
     @staticmethod
     def parravano2011(mlow, mhigh):
@@ -108,7 +141,43 @@ class IMF(object):
         def imf_func(m):
             return (m**(-1.35)) * (1.0 - np.exp(-(m / 0.35)**(0.51 + 1.35)))
 
-        return IMFSample(imf_func, min_mlow, max_mhigh, mlow, mhigh)
+        return IMFSample(imf_func, min_mlow,
+                         max_mhigh, mlow, mhigh,
+                         'parravano2011')
+
+    @staticmethod
+    def maschberger2013(mlow=0.01, mhigh=150.0):
+        class IMFSample2(object):
+            def __init__(self, mu, alpha, beta, name='maschberger2013'):
+                self.mu = mu
+                self.alpha = alpha
+                self.beta = beta
+                self.__name__ = name
+
+            def sample(self, n, seed=None):
+                np.random.seed(seed)
+                i = np.arange(n)
+                u_min = (i + 0) / n
+                u_max = (i + 1) / n
+                u = np.random.uniform(u_min, u_max, size=n)
+                u = np.random.permutation(u)
+
+                def G(m, mu, alpha, beta):
+                    return (1 + (m/mu)**(1-alpha))**(1-beta)
+
+                mu = self.mu
+                alpha = self.alpha
+                beta = self.beta
+                Gml = G(mlow, mu, alpha, beta)
+                Gmu = G(mhigh, mu, alpha, beta)
+                m = ((u * (Gmu - Gml) + Gml)**(1/(1-beta)) - 1)**(1/(1-alpha))
+                m *= mu
+
+                plot_imf(m, 0.01, 150.0, self.__name__)
+
+                return m
+
+        return IMFSample2(0.2, 2.3, 1.4)
 
 
 # -- End of File --
